f38af5c8e66a75d35b48702d01a31707363e1bbe
vandodev <vandoaparecido@hotmail.com>
2023-10-02 08:26:44 -0300

    :sunny: :cloud_with_lightning_and_rain: :partly_sunny: testID screen getByTestId

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -1,11 +1,13 @@
-import { render } from "@testing-library/react-native";
+import { render, screen } from "@testing-library/react-native";
 
 import { Input } from "@components/Input"
 
 describe("Component: Input", () => {
-    it("should  be render without activity indicator", () => {
-      const {debug} = render ( <Input />);
-      //const {debug} = render ( <Input isLoading/>);
-      debug();        
+    it("should  be render without activity indicator if isLoading prop is undefined", () => {
+      //render(<Input isLoading/>) sem o is loading o teste não passa
+      render(<Input />)
+      //o screen é utilizado para fazer busca (query)
+      const activityIndicator = screen.getByTestId('activity-indicator')
+      console.log(activityIndicator)        
     })
 })
