154ed5f00990c4cee913724e82eb6cc7ae8ae764
vandodev <vandoaparecido@hotmail.com>
2023-10-02 07:51:30 -0300

    :sunny: :cloud_with_lightning_and_rain: :partly_sunny: Renderizando o componente no teste

---
+++ src/components/Input/input.test.tsx
@@ -0,0 +1,11 @@
+import { render } from "@testing-library/react-native";
+
+import { Input } from "@components/Input"
+
+describe("Component: Input", () => {
+    it("should  be render without activity indicator", () => {
+        render(
+          <Input />
+        )
+      })
+})
