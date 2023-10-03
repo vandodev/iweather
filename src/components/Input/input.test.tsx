14e3b7b3bd3460b543dd2827399bb55303397530
vandodev <vandoaparecido@hotmail.com>
2023-10-03 07:21:08 -0300

    :sunny: :cloud_with_lightning_and_rain: :partly_sunny: toBeTruthy

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -4,13 +4,14 @@ import { Input } from "@components/Input"
 
 describe("Component: Input", () => {
     it("should  be render without activity indicator if isLoading prop is undefined", () => {
-      //render(<Input isLoading/>) com o getByTestId retorna exeção
       render(<Input />)
-      //o get retorna exeção se não encontra o elemento com parâmetro passado
-      //já o query retorna null
-      //Pesquisar no google tabela gery x get jest
-      //const activityIndicator = screen.getByTestId('activity-indicator')
       const activityIndicator = screen.queryByTestId('activity-indicator')
       expect(activityIndicator).toBeNull();      
+    })
+
+    it("should  be render with activity indicator if isLoading prop is true", () => {
+      render(<Input isLoading />)  
+      const activityIndicator = screen.getByTestId('activity-indicator');
+      expect(activityIndicator).toBeTruthy();
     })
 })
