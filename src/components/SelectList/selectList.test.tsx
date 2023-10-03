1b4f11611022db604861a9ec71df6ea6520d0e78
vandodev <vandoaparecido@hotmail.com>
2023-10-03 07:37:46 -0300

    :sunny: :cloud_with_lightning_and_rain: :partly_sunny: Cidade selecionada com GetByText

---
+++ src/components/SelectList/selectList.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react-native"
+
+import { SelectList } from '@components/SelectList'
+import { CityProps } from "@services/getCityByNameService"
+
+describe("Component: SelectList", () =>{
+  it('should be return city details selected', async() => {
+    const data = [
+      { id: '1', name: 'Morro Agudo', latitude: 123, longitude: 456 },
+      { id: '2', name: 'Ribeirão Preto', latitude: 123, longitude: 456 }
+    ]
+
+// const {debug} = render(
+   render(
+      <SelectList 
+        data={data}
+        onChange={() =>{}}
+        onPress={() => {}}
+      />
+    )
+    // debug()
+    //leva em cosideração case-sensitive
+    const selectedCity = screen.getByText('Morro Agudo')
+    console.log(selectedCity)
+  })
+})
