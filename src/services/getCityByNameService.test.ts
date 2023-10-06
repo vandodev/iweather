6897df499a48d9bcebfa66ccada6049ff413f85d
vandodev <vandoaparecido@hotmail.com>
2023-10-06 08:15:52 -0300

    :sunny: :cloud_with_lightning_and_rain: :partly_sunny: Testando retorna da API

--- src/services/getCityByNameService.test.ts
+++ src/services/getCityByNameService.test.ts
@@ -2,13 +2,13 @@ import { mockCityAPIResponse } from '@__tests__/mocks/mockCityAPIResponse'
 import { api } from './api'
 import { getCityByNameService } from './getCityByNameService'
 
-describe("API: getCityByNameService", () => {
+describe("Service: getCityByNameService", () => {
   it('should return city details', async () => {
  
     jest.spyOn(api, "get").mockResolvedValue({ data: mockCityAPIResponse })
 
     const response = await getCityByNameService('São Paulo')
-    console.log(response)
+    // console.log(response)
     expect(response.length).toBeGreaterThan(0)
   })
 })
