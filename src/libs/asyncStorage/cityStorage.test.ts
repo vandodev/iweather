76475d0cfa1466b9919719336e649c8cd0f8debe
vandodev <vandoaparecido@hotmail.com>
2023-10-07 07:51:27 -0300

    :sunny: :cloud_with_lightning_and_rain: :partly_sunny: Teste do saveStorageCity

--- src/libs/asyncStorage/cityStorage.test.ts
+++ src/libs/asyncStorage/cityStorage.test.ts
@@ -1,4 +1,5 @@
-import { getStorageCity } from "@libs/asyncStorage/cityStorage"
+import { getStorageCity, saveStorageCity } from "@libs/asyncStorage/cityStorage"
+import { CityProps } from "@services/getCityByNameService"
 
 describe('Storage: CityStorage', () => {
   it("should be return null when don't have a city storaged", async () => {
@@ -7,4 +8,28 @@ describe('Storage: CityStorage', () => {
     expect(response).toBeNull()
     // expect(response).toHaveProperty("name")
   })
+
+  it('shoul be return city storaged', async () => {
+    const newCity: CityProps = {
+      id: '1',
+      name:'São Paulo',
+      latitude: 123,
+      longitude: 456
+    }
+
+    await saveStorageCity(newCity)
+
+    const response = await getStorageCity()
+
+    // expect(response).toEqual({
+    //     id: '2',
+    //     name:'Morro Agudo',
+    //     latitude: 123,
+    //     longitude: 321
+    //   }
+    // )
+
+    expect(response).toEqual(newCity)
+  })
+
 })
