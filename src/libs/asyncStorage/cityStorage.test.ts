fa372dee8bf50939295d9872609e54aab5e1a688
vandodev <vandoaparecido@hotmail.com>
2023-10-07 07:33:18 -0300

    :sunny: :cloud_with_lightning_and_rain: :partly_sunny: Mock do Async Storage

--- src/libs/asyncStorage/cityStorage.test.ts
+++ src/libs/asyncStorage/cityStorage.test.ts
@@ -3,5 +3,8 @@ import { getStorageCity } from "@libs/asyncStorage/cityStorage"
 describe('Storage: CityStorage', () => {
   it("should be return null when don't have a city storaged", async () => {
     const response = await getStorageCity()
+    // console.log(response)
+    expect(response).toBeNull()
+    // expect(response).toHaveProperty("name")
   })
 })
