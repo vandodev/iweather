c65ec96b8aa4618467f8e740af1b23830fe57476
vandodev <vandoaparecido@hotmail.com>
2023-10-08 08:50:48 -0300

    :sunny: :cloud_with_lightning_and_rain: :partly_sunny: Teste do removeStorageCity

--- src/libs/asyncStorage/cityStorage.test.ts
+++ src/libs/asyncStorage/cityStorage.test.ts
@@ -1,35 +1,31 @@
-import { getStorageCity, saveStorageCity } from "@libs/asyncStorage/cityStorage"
+import { getStorageCity, saveStorageCity, removeStorageCity } from "@libs/asyncStorage/cityStorage"
 import { CityProps } from "@services/getCityByNameService"
+
+const newCity: CityProps = {
+  id: '1',
+  name:'São Paulo',
+  latitude: 123,
+  longitude: 456
+}
+
 
 describe('Storage: CityStorage', () => {
   it("should be return null when don't have a city storaged", async () => {
     const response = await getStorageCity()
-    // console.log(response)
     expect(response).toBeNull()
-    // expect(response).toHaveProperty("name")
   })
 
   it('shoul be return city storaged', async () => {
-    const newCity: CityProps = {
-      id: '1',
-      name:'São Paulo',
-      latitude: 123,
-      longitude: 456
-    }
-
     await saveStorageCity(newCity)
-
     const response = await getStorageCity()
-
-    // expect(response).toEqual({
-    //     id: '2',
-    //     name:'Morro Agudo',
-    //     latitude: 123,
-    //     longitude: 321
-    //   }
-    // )
-
     expect(response).toEqual(newCity)
+  })
+
+  it('should be remove city storage', async () => {    
+    await saveStorageCity(newCity)
+    await removeStorageCity()
+    const response = await getStorageCity()
+    expect(response).toBeNull()
   })
 
 })
