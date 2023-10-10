95cdbcf47b5e06f9772b5d9a7f4c28987878fb04
vandodev <vandoaparecido@hotmail.com>
2023-10-10 08:56:10 -0300

    :sunny: :cloud_with_lightning_and_rain: :partly_sunny: Wrapper

--- src/contexts/cityContext.test.ts
+++ src/contexts/cityContext.test.ts
@@ -1,10 +1,11 @@
 import { renderHook } from '@testing-library/react-native'
+import { CityProvider } from '@contexts/CityContext'
 
 import { useCity } from '@hooks/useCity'
 
 describe('Context: cityContext', () => {
   it('should be change selected city',  () => {
-    const { result } = renderHook(() => useCity())
-    console.log(result.current.city)
+    const { result } = renderHook(() => useCity(), { wrapper: CityProvider })
+    console.log(result.current)
   })
 })
