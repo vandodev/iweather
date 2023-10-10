5738cf68d02a169d9cafefa19c6727e77e1559a6
vandodev <vandoaparecido@hotmail.com>
2023-10-10 09:21:27 -0300

    :sunny: :cloud_with_lightning_and_rain: :partly_sunny: waitFor e act

--- src/contexts/cityContext.test.ts
+++ src/contexts/cityContext.test.ts
@@ -1,11 +1,11 @@
-import { renderHook } from '@testing-library/react-native'
+import { act, renderHook, waitFor } from '@testing-library/react-native'
 import { CityProvider } from '@contexts/CityContext'
 
 import { useCity } from '@hooks/useCity'
 
 describe('Context: cityContext', () => {
-  it('should be change selected city',  () => {
+  it('should be change selected city', async () => {
     const { result } = renderHook(() => useCity(), { wrapper: CityProvider })
-    console.log(result.current)
+    await waitFor(() => act(() => console.log(result.current))) 
   })
 })
