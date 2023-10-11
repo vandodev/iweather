5e8a2e0c9f395bbe24393e304c053827f1816bc4
vandodev <vandoaparecido@hotmail.com>
2023-10-11 07:56:24 -0300

    :sunny: :cloud_with_lightning_and_rain: :partly_sunny: Atualizando o estado

--- src/contexts/cityContext.test.ts
+++ src/contexts/cityContext.test.ts
@@ -6,6 +6,16 @@ import { useCity } from '@hooks/useCity'
 describe('Context: cityContext', () => {
   it('should be change selected city', async () => {
     const { result } = renderHook(() => useCity(), { wrapper: CityProvider })
-    await waitFor(() => act(() => console.log(result.current))) 
+    // await waitFor(() => act(() => console.log(result.current))) 
+
+    await waitFor(() => act(() => result.current.handleChanceCity({
+      id: '1',
+      name: 'Morro Agudo',
+      latitude: 568,
+      longitude: 194
+    })))
+
+    expect(result.current.city?.name).toBe('Morro Agudo')
+
   })
 })
